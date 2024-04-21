4f56310cb7346796b180644f33be8a7f3f3fcf33
Marko-Shevchuk <126328450+Marko-Shevchuk@users.noreply.github.com>
2024-04-21 19:36:18 +0300

    Complete the official unity 2d roguelike tutorial (#12)
    * Init project, gitignore (#10)
    
    * Delete corrupted project (#11)
    
    * Reinit file structure
    
    * complete the official unity 2d roguelike tutorial

---
+++ roguelike/Assets/Scripts/GameManager.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace Roguelike
+{
+	using System.Collections.Generic;		
+	using UnityEngine.UI;					
+	
+	public class GameManager : MonoBehaviour
+	{
+		public float levelStartDelay = 2f;						
+		public float turnDelay = 0.1f;							//todo fix
+		public int playerFoodPoints = 100;						
+		public static GameManager instance = null;				
+		[HideInInspector] public bool playersTurn = true;		
+		
+		
+		private Text levelText;									
+		private GameObject levelImage;							
+		private BoardManager boardScript;						
+		private int level = 1;									
+		private List<Enemy> enemies;							
+		private bool enemiesMoving;								
+		private bool doingSetup = true;							
+		
+		
+		void Awake()
+		{
+            if (instance == null)
+                instance = this;
+
+            else if (instance != this)
+                Destroy(gameObject);	
+
+			DontDestroyOnLoad(gameObject);
+			
+			enemies = new List<Enemy>();
+			
+			boardScript = GetComponent<BoardManager>();
+
+			InitGame();
+		}
+
+        //this is called only ONCE, and the parameter tells it to be called only after the scene was loaded
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static public void CallbackInitialization()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        //each time a scene is loaded.
+        static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            instance.level++;
+            instance.InitGame();
+        }
+
+		void InitGame()
+		{
+			//prevent player from moving while title card is up
+			doingSetup = true;
+			levelImage = GameObject.Find("LevelImage");
+			levelText = GameObject.Find("LevelText").GetComponent<Text>();
+			levelText.text = "Room " + level;
+
+			levelImage.SetActive(true);
+			
+			Invoke("HideLevelImage", levelStartDelay);
+			
+			//prepare for next level.
+			enemies.Clear();
+
+			boardScript.SetupScene(level);
+			
+		}
+		
+		
+		void HideLevelImage()
+		{
+			levelImage.SetActive(false);
+			doingSetup = false;
+		}
+		
+		void Update()
+		{
+			if(playersTurn || enemiesMoving || doingSetup) //todo fix
+				return;
+			
+			StartCoroutine (MoveEnemies ());
+		}
+		
+		public void AddEnemyToList(Enemy script)
+		{
+			enemies.Add(script);
+		}
+		
+		public void GameOver()
+		{
+			levelText.text = "After " + level + " rooms, you died.";
+			levelImage.SetActive(true);
+			enabled = false;
+		}
+		
+
+		IEnumerator MoveEnemies()
+		{
+			enemiesMoving = true;
+
+			yield return new WaitForSeconds(turnDelay);
+
+			if (enemies.Count == 0) 
+			{
+				yield return new WaitForSeconds(turnDelay);
+			}
+
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				enemies[i].MoveEnemy ();
+				
+				//Wait for Enemy's moveTime before moving next Enemy?
+				yield return new WaitForSeconds(enemies[i].moveTime);
+			}
+			playersTurn = true;
+			enemiesMoving = false;
+		}
+	}
+}
+
---
+++ roguelike/Assets/Scripts/Player.cs
@@ -0,0 +1,209 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;	
+using UnityEngine.SceneManagement;
+
+namespace Roguelike
+{
+	
+	public class Player : MovingObject
+	{
+		public float restartLevelDelay = 1f;		
+		public int pointsPerFood = 10;				
+		public int pointsPerSoda = 20;				
+		public int wallDamage = 1;					
+		public Text foodText;						
+		public AudioClip moveSound1;				
+		public AudioClip moveSound2;				
+		public AudioClip eatSound1;					
+		public AudioClip eatSound2;					
+		public AudioClip drinkSound1;				
+		public AudioClip drinkSound2;				
+		public AudioClip gameOverSound;				
+		
+		private Animator animator;					
+		private int food;                          
+#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+        private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
+#endif
+		
+		
+
+		protected override void Start ()
+		{
+			animator = GetComponent<Animator>();
+			food = GameManager.instance.playerFoodPoints;
+			foodText.text = "Food: " + food;
+			base.Start ();
+		}
+		
+
+		private void OnDisable ()
+		{
+			//When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
+			GameManager.instance.playerFoodPoints = food;
+		}
+		
+		
+		private void Update ()
+		{
+			if(!GameManager.instance.playersTurn) return;
+			
+			int horizontal = 0;  	
+			int vertical = 0;		
+
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+			
+			
+			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+
+			vertical = (int) (Input.GetAxisRaw ("Vertical"));
+			
+
+			if(horizontal != 0)
+			{
+				vertical = 0;
+			}
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+			
+			//Check if Input has registered more than zero touches
+			if (Input.touchCount > 0)
+			{
+				//Store the first touch detected.
+				Touch myTouch = Input.touches[0];
+				
+				//Check if the phase of that touch equals Began
+				if (myTouch.phase == TouchPhase.Began)
+				{
+					//If so, set touchOrigin to the position of that touch
+					touchOrigin = myTouch.position;
+				}
+				
+				//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+				else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+				{
+					//Set touchEnd to equal the position of this touch
+					Vector2 touchEnd = myTouch.position;
+					
+					//Calculate the difference between the beginning and end of the touch on the x axis.
+					float x = touchEnd.x - touchOrigin.x;
+					
+					//Calculate the difference between the beginning and end of the touch on the y axis.
+					float y = touchEnd.y - touchOrigin.y;
+					
+					//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+					touchOrigin.x = -1;
+					
+					//Check if the difference along the x axis is greater than the difference along the y axis.
+					if (Mathf.Abs(x) > Mathf.Abs(y))
+						//If x is greater than zero, set horizontal to 1, otherwise set it to -1
+						horizontal = x > 0 ? 1 : -1;
+					else
+						//If y is greater than zero, set horizontal to 1, otherwise set it to -1
+						vertical = y > 0 ? 1 : -1;
+				}
+			}
+			
+#endif 
+
+			if(horizontal != 0 || vertical != 0)
+			{
+
+				AttemptMove<Wall> (horizontal, vertical);
+			}
+		}
+		
+		protected override void AttemptMove <T> (int xDir, int yDir)
+		{
+			//todo fix
+			food--;
+			
+			foodText.text = "Food: " + food;
+			
+			base.AttemptMove <T> (xDir, yDir);
+			
+
+			RaycastHit2D hit;
+
+			if (Move (xDir, yDir, out hit)) 
+			{
+				SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
+			}
+			
+			CheckIfGameOver ();
+			
+			GameManager.instance.playersTurn = false;
+		}
+		
+		protected override void OnCantMove <T> (T component)
+		{
+			Wall hitWall = component as Wall;
+			hitWall.DamageWall (wallDamage);
+			animator.SetTrigger ("playerChop");
+		}
+		
+		private void OnTriggerEnter2D (Collider2D other)
+		{
+//todo collide with enemy
+			if(other.tag == "Exit")
+			{
+				Invoke ("Restart", restartLevelDelay);
+				
+				//Disable the player object since level is over.
+				enabled = false;
+			}
+			
+			
+			else if(other.tag == "Food")
+			{
+
+				food += pointsPerFood;
+				
+				
+				foodText.text = "+" + pointsPerFood + " Food: " + food;
+				
+				
+				SoundManager.instance.RandomizeSfx (eatSound1, eatSound2);
+				
+				other.gameObject.SetActive (false);
+			}
+			
+			
+			else if(other.tag == "Soda")
+			{
+				food += pointsPerSoda;
+				foodText.text = "+" + pointsPerSoda + " Food: " + food;
+				SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
+				other.gameObject.SetActive (false);
+			}
+		}
+		
+		
+		//reload the scene
+		private void Restart ()
+		{
+			//loading in "Single" mode so it replaces the existing one
+            //it will NOT load all the scene objects in the current scene.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+		}
+		
+		public void LoseFood (int loss)
+		{
+			animator.SetTrigger ("playerHit");
+			food -= loss;
+			foodText.text = "-"+ loss + " Food: " + food;
+			CheckIfGameOver ();
+		}
+		
+		private void CheckIfGameOver ()
+		{
+			if (food <= 0) 
+			{
+				SoundManager.instance.PlaySingle (gameOverSound);
+				SoundManager.instance.musicSource.Stop();
+				GameManager.instance.GameOver ();
+			}
+		}
+	}
+}
+
